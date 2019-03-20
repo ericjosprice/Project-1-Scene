09de21e07c7cf61a7e4845ccd9bc22e97f0c8369
ericjosprice <epriceutexas@gmail.com>
2019-03-20 10:58:11 -0500

    added button to geolocation feature

--- assets/javascript/foursquare.js
+++ assets/javascript/foursquare.js
@@ -15,7 +15,6 @@
 //credentials above
 //===================================================================================
 // use geolocation to get lon and lat
-
 var lat, lon, acc;
 
 var options = {
@@ -23,6 +22,13 @@ var options = {
     timeout: 50000,
     maximumAge: 10
 };
+
+// when user clicks the foursquare button run the geolocation function
+$(document).on("click", "#listing-buttons-left", clickedFourSquare)
+
+
+function clickedFourSquare(){
+
 
 function success(pos) {
     var crd = pos.coords;
@@ -85,8 +91,6 @@ function getFoureSquareData() {
     });
     console.log(sQueryURL);
 
-
-
     // the below code is an attempt to get images from foursquare. this feature will be added if there is time
     // $.ajax({
     //     url: 'https://api.foursquare.com/v2/venues/VENUE_ID=4ce2a3b278ddf04dfde8b198/photos' + creds,
@@ -97,91 +101,93 @@ function getFoureSquareData() {
 }
 
 setTimeout(getFoureSquareData, 4000);
+console.log("yelp button was clicked")
+}
 //===================================================================================
-// Initialize Firebase
-var config = {
-    apiKey: "AIzaSyASKSOl-vh7Ctml-gBjg70xwgNB77GAdco",
-    authDomain: "yayornay-a4231.firebaseapp.com",
-    databaseURL: "https://yayornay-a4231.firebaseio.com",
-    projectId: "yayornay-a4231",
-    storageBucket: "yayornay-a4231.appspot.com",
-    messagingSenderId: "473437635582"
-};
-firebase.initializeApp(config);
+// // Initialize Firebase
+// var config = {
+//     apiKey: "AIzaSyASKSOl-vh7Ctml-gBjg70xwgNB77GAdco",
+//     authDomain: "yayornay-a4231.firebaseapp.com",
+//     databaseURL: "https://yayornay-a4231.firebaseio.com",
+//     projectId: "yayornay-a4231",
+//     storageBucket: "yayornay-a4231.appspot.com",
+//     messagingSenderId: "473437635582"
+// };
+// firebase.initializeApp(config);
 
 
-var database = firebase.database();
+// var database = firebase.database();
 
 
-// Capture Button Click
-$("#submit-btn").on("click", function (event) {
-    event.preventDefault();
-    console.log("clicked submit button")
-    // Grabbed values from text boxes
-    var sCoffeeShop = $("#business-input").val().trim();
-    var sAddress = $("#address-input").val().trim();
-    var sDeal = $("#deal-input").val().trim();
-    var sTime = $("#timeframe-input").val().trim();
-    if (sCoffeeShop === "" ||
-        sAddress === "" ||
-        sDeal === "" ||
-        sTime === "") {
-        alert("Complete all fields to continue.");
-        return;
-    } else {
-        // Code for handling the push
-        database.ref().push({
-            business: sCoffeeShop,
-            address: sAddress,
-            deal: sDeal,
-            time: sTime,
-            dateAdded: firebase.database.ServerValue.TIMESTAMP
-        });
+// // Capture Button Click
+// $("#submit-btn").on("click", function (event) {
+//     event.preventDefault();
+//     console.log("clicked submit button")
+//     // Grabbed values from text boxes
+//     var sCoffeeShop = $("#business-input").val().trim();
+//     var sAddress = $("#address-input").val().trim();
+//     var sDeal = $("#deal-input").val().trim();
+//     var sTime = $("#timeframe-input").val().trim();
+//     if (sCoffeeShop === "" ||
+//         sAddress === "" ||
+//         sDeal === "" ||
+//         sTime === "") {
+//         alert("Complete all fields to continue.");
+//         return;
+//     } else {
+//         // Code for handling the push
+//         database.ref().push({
+//             business: sCoffeeShop,
+//             address: sAddress,
+//             deal: sDeal,
+//             time: sTime,
+//             dateAdded: firebase.database.ServerValue.TIMESTAMP
+//         });
 
-        $("#business-input").val("")
-        $("#address-input").val("")
-        $("#deal-input").val("")
-        $("#timeframe-input").val("")
-    }
-});
+//         $("#business-input").val("")
+//         $("#address-input").val("")
+//         $("#deal-input").val("")
+//         $("#timeframe-input").val("")
+//     }
+// });
 
-// Firebase watcher .on("child_added"
-database.ref().on("child_added", function (childSnapshot) {
-    // storing the snapshot.val() in a variable for convenience
-    var sv = childSnapshot.val();
+// // Firebase watcher .on("child_added"
+// database.ref().on("child_added", function (childSnapshot) {
+//     // storing the snapshot.val() in a variable for convenience
+//     var sv = childSnapshot.val();
 
-    // Console.loging the last user's data
-    console.log(sv);
-    console.log(sv.business);
-    console.log(sv.address);
-    console.log(sv.deal);
-    console.log(sv.time);
-    // console.log("moment: " + moment().startOf(sv.time).fromNow());
-    // console.log("minutes till train: " + moment(sv.time, 'HH:mm').diff(moment(), "minutes"))
+//     // Console.loging the last user's data
+//     console.log(sv);
+//     console.log(sv.business);
+//     console.log(sv.address);
+//     console.log(sv.deal);
+//     console.log(sv.time);
+//     // console.log("moment: " + moment().startOf(sv.time).fromNow());
+//     // console.log("minutes till train: " + moment(sv.time, 'HH:mm').diff(moment(), "minutes"))
 
-    // store time variable for display
-    // var sArrival = moment(sv.time, 'HH:mm').format('hh:mm a');
-    // var sMinutes = moment(sv.time, 'HH:mm').diff(moment(), "minutes");
+//     // store time variable for display
+//     // var sArrival = moment(sv.time, 'HH:mm').format('hh:mm a');
+//     // var sMinutes = moment(sv.time, 'HH:mm').diff(moment(), "minutes");
 
-    // if (sMinutes <= 0) {
-    //     sMinutes = "Departed";
-    // }
+//     // if (sMinutes <= 0) {
+//     //     sMinutes = "Departed";
+//     // }
 
 
-    $("#feed").prepend("<div class='jumbotron'><div id='bar-name'>" +
-        sv.business +
-        " </div><div id='bar-location'>" + sv.address +
-        " </div><div id='deal-description'>" + sv.deal +
-        //convert sv.time to a usable format for moment.js then format for display in hh, which displays hours in standard time
-        " </div><div id='deal-timeframe'>" + sv.time +
-        " </div><div id='listing-buttons-left'></div><a class= 'listing-button' href='#'><i class='fab fa-yelp'></i></a><a class= 'listing-button' href='#'><i class='fas fa-map-marker-alt'></i></a></div><div id='listing-buttons-right'><a class= 'listing-button' href='#'><i class='fas fa-thumbs-up'></i></a><div class= 'listing-value'></div><a class= 'listing-button' href='#'><i class='fas fa-thumbs-down'></i></a><div class= 'listing-value'></div></div></div></div>");
-    //   $("#train-table").append(row)
-    //   $("#train-name-display").text(sv.name);
-    //   $("#destination-display").text(sv.destination);
-    //   $("#time-display").text(sv.time);
-    //   $("#frequency-display").text(sv.frequency);
+//     $("#feed").prepend("<div class='jumbotron'><div id='bar-name'>" +
+//         sv.business +
+//         " </div><div id='bar-location'>" + sv.address +
+//         " </div><div id='deal-description'>" + sv.deal +
+//         //convert sv.time to a usable format for moment.js then format for display in hh, which displays hours in standard time
+//         " </div><div id='deal-timeframe'>" + sv.time +
+//         " </div><div id='listing-buttons-left'></div><a class= 'listing-button' href='#'><i class='fab fa-yelp'></i></a><a class= 'listing-button' href='#'><i class='fas fa-map-marker-alt'></i></a></div><div id='listing-buttons-right'><a class= 'listing-button' href='#'><i class='fas fa-thumbs-up'></i></a><div class= 'listing-value'></div><a class= 'listing-button' href='#'><i class='fas fa-thumbs-down'></i></a><div class= 'listing-value'></div></div></div></div>");
+//     //   $("#train-table").append(row)
+//     //   $("#train-name-display").text(sv.name);
+//     //   $("#destination-display").text(sv.destination);
+//     //   $("#time-display").text(sv.time);
+//     //   $("#frequency-display").text(sv.frequency);
 
-    // Handle the errors
-}, function (errorObject) {
-    console.log("Errors handled: " + errorObject.code);
-});
+//     // Handle the errors
+// }, function (errorObject) {
+//     console.log("Errors handled: " + errorObject.code);
+// });
