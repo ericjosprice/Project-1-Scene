603ba56a20d3f65da26196fc17c5a30528c6c9dd
George Arredondo <arredondo.george@gmail.com>
2019-03-22 14:40:10 -0500

    adding the changes i made to the logic js file-- i am able to show the submit button when the user inputs all the fields and then submit to be stored on firebase.

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -15,9 +15,9 @@ $(document).ready(function () {
   ////////////////////////////
   // SUBMIT BUTTON FUNCTION //
   ////////////////////////////
+
   $("#form-submit").on("click", function () {
     event.preventDefault();
-    $("#post-form").addClass("hide");
 
     // Form inputs
     var businessInput = $("#business-input").val().trim();
@@ -27,35 +27,50 @@ $(document).ready(function () {
     var likeCounter = 0;
     var dislikeCounter = 0;
 
-    if (businessInput === "" ||
-      addressInput === "" ||
-      dealInput === "" ||
-      timeframeInput === "") {
-      alert("Complete all fields to continue.");
-      
 
-      return;
-    } else {
-      // Code for handling the push
-      database.ref().push({
-        business: businessInput,
-        address: addressInput,
-        deal: dealInput,
-        time: timeframeInput,
-        dateAdded: firebase.database.ServerValue.TIMESTAMP,
-        like: likeCounter,
-        dislike: dislikeCounter
-      });
+    $("#form-submit").disabled = true;
+    $("#form-submit").val("Submit");
+    // Code for handling the push
+    database.ref().push({
+      business: businessInput,
+      address: addressInput,
+      deal: dealInput,
+      time: timeframeInput,
+      dateAdded: firebase.database.ServerValue.TIMESTAMP,
+      like: likeCounter,
+      dislike: dislikeCounter
+    });
 
-      // clearing the forms after submitting
-      $("#business-input").val("")
-      $("#address-input").val("")
-      $("#deal-input").val("")
-      $("#timeframe-input").val("")
-    }
+    // clearing the forms after submitting
+    $("#business-input").val("")
+    $("#address-input").val("")
+    $("#deal-input").val("")
+    $("#timeframe-input").val("")
+
   });
 
   var likeClickVal = 0;
+
+
+  // button hide and show below
+
+  $("#form-submit").disabled = true;
+  $("#form-submit").hide();
+
+  $(".form-control").on("change", function () {
+    event.preventDefault();
+
+    if ($("#business-input").val() != "" &&
+      $("#address-input").val() != "" &&
+      $("#deal-input").val() != "" &&
+      $("#timeframe-input").val() != "") {
+      $("#form-submit").show();
+      $("#form-submit").disabled = false;
+      $("#ins").hide();
+    }
+  });
+
+
 
 
 
@@ -128,61 +143,61 @@ $(document).ready(function () {
   ////////////////////////////////////////
   // Firebase watcher .on("child_added" //
   ////////////////////////////////////////
-    database.ref().on("child_added", function (childSnapshot) {
-      // storing the snapshot.val() in a variable for convenience
-      var sv = childSnapshot.val();
-      var key = childSnapshot.key;
+  database.ref().on("child_added", function (childSnapshot) {
+    // storing the snapshot.val() in a variable for convenience
+    var sv = childSnapshot.val();
+    var key = childSnapshot.key;
 
-      console.log(sv);
-      console.log(key);
-      // HTML elements created with jQuery 
-      //all cards and their elements have been given a unique ID
-      var jumbotron = $("<div>").addClass("jumbotron");
-      jumbotron.attr("id", "jumbotron" + key);
-      var post = $("<div>").addClass("post");
-      post.attr("id", "post" + key);
-      var businessDisplay = $("<div>").addClass("business-display").text(sv.business);
-      businessDisplay.attr("id", "business-display" + key)
-      var addressDisplay = $("<div>").addClass("address-display").text(sv.address);
-      addressDisplay.attr("id", "address-display" + key);
-      var dealDisplay = $("<div>").addClass("deal-display").text(sv.deal);
-      dealDisplay.attr("id", "deal-display" + key);
-      var timeframeDisplay = $("<div>").addClass("timeframe-display").text(sv.time);
-      timeframeDisplay.attr("id", "timeframe-display" + key);
-      var listingButtons = $("<div>").addClass("listing-buttons");
-      listingButtons.attr("id", "listing-buttons" + key);
-      //added get-directions + key so that toggle class on "this" will work
-      var getDirections = $("<div>").attr("id", "get-directions" + key).addClass("fas fa-location-arrow listing-button get-directions");
-      getDirections.attr("data-item", key);
-      var thumbsUp = $("<div>").attr("id", "thumbs-up" + key).addClass("fas fa-thumbs-up listing-button thumbs-up");
-      var thumbsUpCount = $("<div>").attr("id", "thumbs-up-count-" + key).addClass("listing-value" + " " + key).text(sv.like);
-      var thumbsDown = $("<div>").attr("data-id", "0").addClass("fas fa-thumbs-down listing-button thumbs-down" + " " + key);
-      var thumbsDownCount = $("<div>").attr("id", "thumbs-down-count-" + key).addClass("listing-value" + " " + key).text(sv.dislike);
-      thumbsUp.attr("data-item", key);
-      thumbsDown.attr("data-item", key);
+    console.log(sv);
+    console.log(key);
+    // HTML elements created with jQuery 
+    //all cards and their elements have been given a unique ID
+    var jumbotron = $("<div>").addClass("jumbotron");
+    jumbotron.attr("id", "jumbotron" + key);
+    var post = $("<div>").addClass("post");
+    post.attr("id", "post" + key);
+    var businessDisplay = $("<div>").addClass("business-display").text(sv.business);
+    businessDisplay.attr("id", "business-display" + key)
+    var addressDisplay = $("<div>").addClass("address-display").text(sv.address);
+    addressDisplay.attr("id", "address-display" + key);
+    var dealDisplay = $("<div>").addClass("deal-display").text(sv.deal);
+    dealDisplay.attr("id", "deal-display" + key);
+    var timeframeDisplay = $("<div>").addClass("timeframe-display").text(sv.time);
+    timeframeDisplay.attr("id", "timeframe-display" + key);
+    var listingButtons = $("<div>").addClass("listing-buttons");
+    listingButtons.attr("id", "listing-buttons" + key);
+    //added get-directions + key so that toggle class on "this" will work
+    var getDirections = $("<div>").attr("id", "get-directions" + key).addClass("fas fa-location-arrow listing-button get-directions");
+    getDirections.attr("data-item", key);
+    var thumbsUp = $("<div>").attr("id", "thumbs-up" + key).addClass("fas fa-thumbs-up listing-button thumbs-up");
+    var thumbsUpCount = $("<div>").attr("id", "thumbs-up-count-" + key).addClass("listing-value" + " " + key).text(sv.like);
+    var thumbsDown = $("<div>").attr("data-id", "0").addClass("fas fa-thumbs-down listing-button thumbs-down" + " " + key);
+    var thumbsDownCount = $("<div>").attr("id", "thumbs-down-count-" + key).addClass("listing-value" + " " + key).text(sv.dislike);
+    thumbsUp.attr("data-item", key);
+    thumbsDown.attr("data-item", key);
 
-      var directionsContainer = $("<div>").attr("id", "directions-container" + key).addClass("hide directions-container");
+    var directionsContainer = $("<div>").attr("id", "directions-container" + key).addClass("hide directions-container");
 
 
-      var directions = $("<div>").addClass("directions data-directions").text("");
-      directions.attr("id", "directions" + key);
-      var closeDirections = $("<div>").attr("id", "close-directions" + key).addClass("far fa-times-circle listing-button close-directions").attr("key-value", key);
+    var directions = $("<div>").addClass("directions data-directions").text("");
+    directions.attr("id", "directions" + key);
+    var closeDirections = $("<div>").attr("id", "close-directions" + key).addClass("far fa-times-circle listing-button close-directions").attr("key-value", key);
 
-      // creating the post
-      $("#feed").prepend(jumbotron);
-      jumbotron.append(post);
-      post.append(businessDisplay, addressDisplay, dealDisplay, timeframeDisplay);
-      post.append("<hr>");
-      post.append(listingButtons);
-      listingButtons.append(getDirections, thumbsUp, thumbsUpCount, thumbsDown, thumbsDownCount);
-      jumbotron.append(directionsContainer);
-      directionsContainer.append("<hr>");
-      directionsContainer.append(directions, closeDirections);
-    
-    }, function(errorObject) {
-      console.log("The read failed: " + errorObject.code);
+    // creating the post
+    $("#feed").prepend(jumbotron);
+    jumbotron.append(post);
+    post.append(businessDisplay, addressDisplay, dealDisplay, timeframeDisplay);
+    post.append("<hr>");
+    post.append(listingButtons);
+    listingButtons.append(getDirections, thumbsUp, thumbsUpCount, thumbsDown, thumbsDownCount);
+    jumbotron.append(directionsContainer);
+    directionsContainer.append("<hr>");
+    directionsContainer.append(directions, closeDirections);
 
-    })
+  }, function (errorObject) {
+    console.log("The read failed: " + errorObject.code);
+
+  })
 
   // Click functions for navigating the document
   $("#create-post").on("click", function () {
@@ -208,7 +223,7 @@ $(document).ready(function () {
   $(document).on("click", ".close-directions", function () {
     var sKey = $(this).attr("key-value")
     console.log("close has been clicked for key: " + sKey);
-    $("#directions-container"+ sKey).addClass("hide");
+    $("#directions-container" + sKey).addClass("hide");
   });
 
 });
