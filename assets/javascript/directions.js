872f024877382ed156934a015c91a3e1dd64646d
George Arredondo <arredondo.george@gmail.com>
2019-03-21 20:16:04 -0500

    Replaced the # in the address with %23. Line 41 of directions.js

--- assets/javascript/directions.js
+++ assets/javascript/directions.js
@@ -13,11 +13,6 @@ $(document).ready(function () {
 
         function success(pos) {
             var crd = pos.coords;
-
-            // console.log('Your current position is:');
-            // console.log(`Latitude : ${crd.latitude}`);
-            // console.log(`Longitude: ${crd.longitude}`);
-            // console.log(`More or less ${crd.accuracy} meters.`);
             var lat = crd.latitude;
             var lon = crd.longitude;
             acc = crd.accuracy;
@@ -40,17 +35,13 @@ $(document).ready(function () {
 
         $(".directions-container"+ key).removeClass("hide");
 
-        // str = str.replace(/\s+/g, '');
-        console.log("BusStreetAddress " + BusStreetAddress.replace(/\s+/g, '+'));
+        // console.log("BusStreetAddress " + BusStreetAddress.replace(/\s+/g, '+'));
         // add the city and state to the BusStreeAddress
 
-        busAddress = BusStreetAddress.replace(/\s+/g, '+') + "+Austin" + "+TX"
+        busAddress = BusStreetAddress.replace(/\s#/g, '%23') + "+Austin" + "+TX"
 
         // generating the queryURL for the AJAX call
         var directionsQueryURL = "https://cors-ut-bootcamp.herokuapp.com/https://maps.googleapis.com/maps/api/directions/json?origin=" + userOrigin + "&destination=" + busAddress + "&mode=walking&key=AIzaSyAWVvEMYJEt9Bq0oqqA1FE156FHs86msTg"
-
-
-        console.log(directionsQueryURL);
 
         // ajax call
         $.ajax({
@@ -63,10 +54,6 @@ $(document).ready(function () {
             console.log(response);
             var routeArray = response.routes[0].legs[0].steps
 
-            // var routeArray = response.routes[1].legs[4].steps
-
-            // console.log("This should be the routeArray " + routeArray);
-
             // this for loop, cycles through the array and pulls out the HTML walking directions 
             for (var i = 0; i < routeArray.length; i++) {
                 console.log(routeArray[i].html_instructions);
@@ -74,14 +61,10 @@ $(document).ready(function () {
                 // puts each of the legs into a <p> tag so that they can be display one-by-one rather than as an entire string
                 var directionsToBusiness = $("<p>" + routeArray[i].html_instructions + "</p>");
 
-                // console.log(directionsToBusiness);
                 // appends each leg with a <p> to the walkingJourney div created up top
                 walkingJourney.append(directionsToBusiness);
-                // console.log(walkingJourney);
-                // calls the HTML id to display the directions on the DOM
-                // $("#directions").html(walkingJourney);
-                // $("#directions-container").html(walkingJourney);
 
+                // calls the HTML id to display the directions on the DOM                
                 $(".data-directions" + key).html(walkingJourney);
             }
 
