59b60a7be05d862b642b87c96730e3560b73cd00
lqdsqd <wesley.lebert@gmail.com>
2019-03-24 13:08:35 -0500

    deleted old code and console logs

--- assets/javascript/foursquare.js
+++ assets/javascript/foursquare.js
@@ -12,16 +12,10 @@ $(document).on("click", "#foursquare-button", locationConsent)
 
 
 function locationConsent() {
-    console.log("button clicked")
     navigator.geolocation.getCurrentPosition(success);
 
     function success(pos) {
         var crd = pos.coords;
-
-        // console.log('Your current position is:');
-        // console.log(`Latitude : ${crd.latitude}`);
-        // console.log(`Longitude: ${crd.longitude}`);
-        // console.log(`More or less ${crd.accuracy} meters.`);
         lat = crd.latitude;
         lon = crd.longitude;
         acc = crd.accuracy;
@@ -29,64 +23,22 @@ function locationConsent() {
     }
 }
 
-// function success(pos) {
-//     var crd = pos.coords;
-
-//     console.log('Your current position is:');
-//     console.log(`Latitude : ${crd.latitude}`);
-//     console.log(`Longitude: ${crd.longitude}`);
-//     console.log(`More or less ${crd.accuracy} meters.`);
-//     lat = crd.latitude;
-//     lon = crd.longitude;
-//     acc = crd.accuracy;
-//     display();
-// }
-
-// function error(err) {
-//     console.warn(`ERROR(${err.code}): ${err.message}`);
-// }
-// // run geolocation code. success, failure, and the last argument failure. 
-// navigator.geolocation.getCurrentPosition(success, error, options);
-
-// // display information on page
-// function display() {
-//     $("#lat").text(lat);
-//     $("#lon").text(lon);
-//     $("#accuracy").text(acc);
-//     //userOrigin defined in George's logic.js file
-//     //uncomment next line once George
-//     // userOrigin(lat, lon);
-// }
-
-
-// ==================================================================================
-//== get foursquare data
-
 function getFourSquareData() {
 
     var creds = "client_id=FEMSVD3N0IBIMAYZZU0DDZK3FUL3RPUI10XOPQ2YA3555ASP&client_secret=K0H2FTFUQJM2LXRZBZVTJFADEK0K0HJZXNAXPWJFI3VK3E5P";
 
     var sQueryURL = 'https://api.foursquare.com/v2/venues/search?v=20180323&limit=5&ll=' + lat + ',' + lon + '&radius=900&categoryId=4d4b7105d754a06376d81259&'+ creds;
 
-    // // nightlifespot seach: 4d4b7105d754a06376d81259
-    // var testing ='https://api.foursquare.com/v2/venues/categories/search?' + creds + '&v=20180323&limit=5&ll=' + lat + ',' + lon + 'radius=1600'
-
-    // var snightlifeQueryURL = 'https://api.foursquare.com/v2/venues/categories/4d4b7105d754a06376d81259/' + creds + '&v=20180323&limit=5&ll=' + lat + ',' + lon + 'radius=1600' 
-
-
-    // https://api.foursquare.com/v2/venues/search?ll="+ ll+"&radius=10000&limit=50&categoryId="+catID+"&client_secret=xxx&client_id=xxx
     $.ajax({
         url: sQueryURL,
         method: "GET"
     }).then(function (response) {
         $("#foursquare-data").empty();
-        console.log(response);
         for (var i = 0; i < response.response.venues.length; i++) {
             var newDiv = $("<div>").addClass("foursquare-name");
             var newP = $("<p>");
             newDiv.attr("id", i);
             newDiv.text(response.response.venues[i].name)
-            // =======
             
             var FourSqBusAddress = response.response.venues[i].location.address;
 
@@ -97,12 +49,8 @@ function getFourSquareData() {
 
                 var displayHyplink = `<p><a href="${linkToGMaps}" target='_blank'>${FourSqBusAddress}</a></p>`
 
-                console.log(linkToGMaps);
                 newP.append(displayHyplink);
             }
-
-            // =======
-
 
             newP.append("<hr>");
             // newDiv.append(newP);
@@ -115,99 +63,4 @@ function getFourSquareData() {
 
 
     });
-    console.log(sQueryURL);
-
-    // the below code is an attempt to get images from foursquare. this feature will be added if there is time
-    // $.ajax({
-    //     url: 'https://api.foursquare.com/v2/venues/VENUE_ID=4ce2a3b278ddf04dfde8b198/photos' + creds,
-    //     method: "GET"
-    // }).then(function (response) {
-    //     console.log(response);
-    // });
 }
-    //===================================================================================
-    // // Initialize Firebase
-    // var config = {
-    //     apiKey: "AIzaSyASKSOl-vh7Ctml-gBjg70xwgNB77GAdco",
-    //     authDomain: "yayornay-a4231.firebaseapp.com",
-    //     databaseURL: "https://yayornay-a4231.firebaseio.com",
-    //     projectId: "yayornay-a4231",
-    //     storageBucket: "yayornay-a4231.appspot.com",
-    //     messagingSenderId: "473437635582"
-    // };
-    // firebase.initializeApp(config);
-
-
-    // var database = firebase.database();
-
-
-    // // Capture Button Click
-    // $("#submit-btn").on("click", function (event) {
-    //     event.preventDefault();
-    //     console.log("clicked submit button")
-    //     // Grabbed values from text boxes
-    //     var sCoffeeShop = $("#business-input").val().trim();
-    //     var sAddress = $("#address-input").val().trim();
-    //     var sDeal = $("#deal-input").val().trim();
-    //     var sTime = $("#timeframe-input").val().trim();
-    //     if (sCoffeeShop === "" ||
-    //         sAddress === "" ||
-    //         sDeal === "" ||
-    //         sTime === "") {
-    //         alert("Complete all fields to continue.");
-    //         return;
-    //     } else {
-    //         // Code for handling the push
-    //         database.ref().push({
-    //             business: sCoffeeShop,
-    //             address: sAddress,
-    //             deal: sDeal,
-    //             time: sTime,
-    //             dateAdded: firebase.database.ServerValue.TIMESTAMP
-    //         });
-
-    //         $("#business-input").val("")
-    //         $("#address-input").val("")
-    //         $("#deal-input").val("")
-    //         $("#timeframe-input").val("")
-    //     }
-    // });
-
-    // // Firebase watcher .on("child_added"
-    // database.ref().on("child_added", function (childSnapshot) {
-    //     // storing the snapshot.val() in a variable for convenience
-    //     var sv = childSnapshot.val();
-
-    //     // Console.loging the last user's data
-    //     console.log(sv);
-    //     console.log(sv.business);
-    //     console.log(sv.address);
-    //     console.log(sv.deal);
-    //     console.log(sv.time);
-    //     // console.log("moment: " + moment().startOf(sv.time).fromNow());
-    //     // console.log("minutes till train: " + moment(sv.time, 'HH:mm').diff(moment(), "minutes"))
-
-    //     // store time variable for display
-    //     // var sArrival = moment(sv.time, 'HH:mm').format('hh:mm a');
-    //     // var sMinutes = moment(sv.time, 'HH:mm').diff(moment(), "minutes");
-
-    //     // if (sMinutes <= 0) {
-    //     //     sMinutes = "Departed";
-    //     // }
-
-
-    //     $("#feed").prepend("<div class='jumbotron'><div id='bar-name'>" +
-    //         sv.business +
-    //         " </div><div id='bar-location'>" + sv.address +
-    //         " </div><div id='deal-description'>" + sv.deal +
-    //         //convert sv.time to a usable format for moment.js then format for display in hh, which displays hours in standard time
-    //         " </div><div id='deal-timeframe'>" + sv.time +
-    //         " </div><div id='listing-buttons-left'></div><a class= 'listing-button' href='#'><i class='fab fa-yelp'></i></a><a class= 'listing-button' href='#'><i class='fas fa-map-marker-alt'></i></a></div><div id='listing-buttons-right'><a class= 'listing-button' href='#'><i class='fas fa-thumbs-up'></i></a><div class= 'listing-value'></div><a class= 'listing-button' href='#'><i class='fas fa-thumbs-down'></i></a><div class= 'listing-value'></div></div></div></div>");
-    //     //   $("#train-table").append(row)
-    //     //   $("#train-name-display").text(sv.name);
-    //     //   $("#destination-display").text(sv.destination);
-    //     //   $("#time-display").text(sv.time);
-    //     //   $("#frequency-display").text(sv.frequency);
-
-    // setTimeout(getFourSquareData, 4000);
-
